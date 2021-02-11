014483f7f2b7541a540201342a989ed75599d784
James Sinkler <jamessinkler713@gmail.com>
2021-02-11 07:44:05 -0800

    scrapes a weather set and display

--- controllers/nprController.js
+++ controllers/nprController.js
@@ -5,25 +5,24 @@ const cheerio = require('cheerio')
 
 
 router.get('/', async(req, res)=> {
-  // scrape npr for coronavirus map
   const response = await fetch('https://www.npr.org/sections/health-shots/2020/09/01/816707182/map-tracking-the-spread-of-the-coronavirus-in-the-u-s')
   const html = await response.text()
   const $ = cheerio.load(html)
-  //got text, map looks like it is embedded in iframe
   const storyTitle = $('.storytitle').html()
-  console.log(storyTitle)
-  console.log('***********************')
   const iFrameDiv = $('#res833624014').html()
   const anotherResponse = await fetch('https://www.npr.org/')
   const anotherHtml = await anotherResponse.text()
   const $$ = cheerio.load(anotherHtml)
   const anotherNprStory = $$('.hp-item').html()
-  console.log('***********************')
-  console.log(anotherNprStory)
+  const weatherResponse = await fetch('https://weather.com/weather/today/l/5b2c46be39eb9b7fc685c9b8018dc90ac51b8401428893251925bff93f607e7f')
+  const weatherHtml = await weatherResponse.text()
+  const $$$ = cheerio.load(weatherHtml)
+  const currentConditionsHeader = $$$('.Card--card--4VS_Q').html()
   res.render('npr', {
     anotherNprStory,
     storyTitle,
-    iFrameDiv
+    iFrameDiv,
+    currentConditionsHeader
   })
 })
 
